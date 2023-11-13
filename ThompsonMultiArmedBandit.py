29bcee651f53cab61b5bdd47ac60d725596fa606
Brent Edwards <chipuni@comcast.net>
2023-11-12 20:21:52 -0800

    Update to ThompsonMultiArmedBandit

--- ThompsonMultiArmedBandit.py
+++ ThompsonMultiArmedBandit.py
@@ -7,6 +7,12 @@ import time
 
 class ThompsonMultiArmedBandit(MultiArmedBandit):
     def choose_side(self):
+        # We need at least one hit on each side for this chooser.
+        if self.generator1.get_hits() == 0:
+            return 1
+        if self.generator2.get_hits() == 0:
+            return 2
+
         # The probability that a side should be chosen should match the probability that the
         # side has the higher average.
         mean1 = self.generator1.mean()
@@ -18,9 +24,9 @@ class ThompsonMultiArmedBandit(MultiArmedBandit):
         nobs2 = self.generator2.get_attempts()
 
         # What is the chance that generator1's mean is greater than generator2's mean?
-        probability = scipy.stats.ttest_ind_from_stats(mean1=mean1, std1=std1, nobs1=nobs1, mean2=mean2, std2=std2, nobs2=nobs2, equal_var=False, alternative='less').pvalue
+        probability = scipy.stats.ttest_ind_from_stats(mean1=mean1, std1=std1, nobs1=nobs1, mean2=mean2, std2=std2, nobs2=nobs2, equal_var=True, alternative='less').pvalue
         chooser = Generator(probability, time.time_ns())
         if chooser.choose():
-            return 1
-        else:
             return 2
+        else:
+            return 1
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from Generator import Generator
 from SimplestMultiArmedBandit import SimplestMultiArmedBandit
 from ThompsonMultiArmedBandit import ThompsonMultiArmedBandit
 
+NUM_ATTEMPTS = 10000
+
 
 def count_hits(predictor: Assayer, attempts):
     for i in range(attempts):
@@ -17,10 +19,36 @@ def count_hits(predictor: Assayer, attempts):
     return predictor.get_hits()
 
 
-NUM_ATTEMPTS = 10000
+def display_results(smad1_hits, larger, larger_generator : Generator, smaller, smaller_generator : Generator, final_choice, correct_choice):
+    print(f"Comparing generators with fraction hits at {smaller} and {larger}.")
+    print(f"Multi-armed bandit hits {smad1_hits}")
+    print(f"Larger generator side: {larger_generator.get_hits()} / {larger_generator.get_attempts()}")
+    print(f"Smaller generator side: {smaller_generator.get_hits()} / {smaller_generator.get_attempts()}")
+    print(f"Final choice: {final_choice} (should be {correct_choice})")
+
+    print()
 
 
-def main(strMAB):
+def multi_armed_bandit_factory(str_mab, firstGenerator, secondGenerator, param):
+    if str_mab == "Simplest":
+        return SimplestMultiArmedBandit(firstGenerator, secondGenerator)
+    elif str_mab == "Epsilon":
+        return EpsilonGreedyMultiArmedBandit(firstGenerator, secondGenerator, float(param))
+    elif str_mab == "Thompson":
+        return ThompsonMultiArmedBandit(firstGenerator, secondGenerator)
+    elif str_mab == "Beta":
+        return BetaBinomialMultiArmedBandit(firstGenerator, secondGenerator, float(param))
+    elif str_mab == "Fixed":
+        return FixedExplorationMultiArmedBandit(firstGenerator, secondGenerator, int(param))
+    print(f"Unknown multi-armed bandit requested: {str_mab}")
+    return None
+
+
+def main(str_mab, param):
+    hits = 0
+    smaller_hits = 0
+    larger_hits = 0
+
     correct_choices = Assayer()
 
     for rerun in range(30):
@@ -29,53 +57,57 @@ def main(strMAB):
 
             smaller_generator = Generator(smaller, 12345 + rerun)
 
-            for add in [0.001, 0.002, 0.005, 0.01, 0.02, 0.05]:
+            for add in [0.005, 0.01, 0.02, 0.05]:
+                smaller_generator.reset()
+
                 larger = smaller + add
                 larger_generator = Generator(larger, 67890 + rerun)
 
-                smad1 = multi_armed_bandit_factory(strMAB, smaller_generator, larger_generator)
-                smad1_hits = count_hits(smad1, NUM_ATTEMPTS)
-                final_choice = smad1.choose_side()
-                correct_choices.train(final_choice == 2)
-                # display_results(smad1_hits, larger, larger_generator, smaller, smaller_generator, final_choice, 2)
+                mab1 = multi_armed_bandit_factory(str_mab, smaller_generator, larger_generator, param)
+                mab1_hits = count_hits(mab1, NUM_ATTEMPTS)
+                hits += mab1_hits
 
-                smad2 = multi_armed_bandit_factory(strMAB, larger_generator, smaller_generator)
-                smad2_hits = count_hits(smad2, NUM_ATTEMPTS)
-                final_choice = smad2.choose_side()
+                # Did it choose the correct side?
+                final_choice = mab1.choose_side()
+                correct_choices.train(final_choice == 2)
+
+                # display_results(mab1_hits, larger, larger_generator, smaller, smaller_generator, final_choice, 2)
+
+                smaller_generator.reset()
+                smaller_hits += count_hits(smaller_generator, NUM_ATTEMPTS)
+
+                larger_generator.reset()
+                larger_hits += count_hits(larger_generator, NUM_ATTEMPTS)
+
+                smaller_generator.reset()
+                larger_generator.reset()
+
+                mab2 = multi_armed_bandit_factory(str_mab, larger_generator, smaller_generator, param)
+                mab2_hits = count_hits(mab2, NUM_ATTEMPTS)
+                hits += mab2_hits
+
+                # Did it choose the correct side?
+                final_choice = mab2.choose_side()
                 correct_choices.train(final_choice == 1)
-                # display_results(smad2_hits, larger, larger_generator, smaller, smaller_generator, final_choice, 1)
+
+                # display_results(mab2_hits, larger, larger_generator, smaller, smaller_generator, final_choice, 1)
+
+                smaller_generator.reset()
+                smaller_hits += count_hits(smaller_generator, NUM_ATTEMPTS)
+
+                larger_generator.reset()
+                larger_hits += count_hits(larger_generator, NUM_ATTEMPTS)
+
+                smaller_generator.reset()
+                larger_generator.reset()
+
 
     print(f"Final correct choices: {correct_choices.get_hits()} / {correct_choices.get_attempts()}")
-
-
-def display_results(smad1_hits, larger, larger_generator, smaller, smaller_generator, final_choice, correct_choice):
-    print(f"Comparing generators with fraction hits at {smaller} and {larger}.")
-    print(f"Multi-armed bandit hits {smad1_hits}")
-    print(f"Final choice: {final_choice} (should be {correct_choice})")
-
-    smaller_generator.reset()
-    smaller_hits = count_hits(smaller_generator, NUM_ATTEMPTS)
-    print(f"Smaller generator hits {smaller_hits}")
-    larger_generator.reset()
-    larger_hits = count_hits(larger_generator, NUM_ATTEMPTS)
-    print(f"Larger generator hits {larger_hits}")
-    print()
-
-
-def multi_armed_bandit_factory(strMAB, firstGenerator, secondGenerator):
-    if strMAB == "Simplest":
-        return SimplestMultiArmedBandit(firstGenerator, secondGenerator)
-    elif strMAB == "Epsilon":
-        return EpsilonGreedyMultiArmedBandit(firstGenerator, secondGenerator, 0.04)
-    elif strMAB == "Thompson":
-        return ThompsonMultiArmedBandit(firstGenerator, secondGenerator)
-    elif strMAB == "Beta":
-        return BetaBinomialMultiArmedBandit(firstGenerator, secondGenerator, 0.95)
-    elif strMAB == "Fixed":
-        return FixedExplorationMultiArmedBandit(firstGenerator, secondGenerator, 200)
-    print(f"Unknown multi-armed bandit requested: {strMAB}")
-    return None
-
+    print(f"Final hits: {hits}. The smaller would be {smaller_hits} and the larger would be {larger_hits}.")
+    print(f"This gives an accuracy of {(hits - smaller_hits) / (larger_hits - smaller_hits)}")
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    if len(sys.argv) > 2:
+        main(sys.argv[1], sys.argv[2])
+    else:
+        main(sys.argv[1], None)
